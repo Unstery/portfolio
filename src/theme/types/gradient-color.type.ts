2da49bcac0b9b4b01f7676d6aec5d365d0b385d8
Unstery <fst09bastien@gmail.com>
2025-08-19 18:29:46 +0200

    chore(tailwind-theme): deleting all old theme configs from tailwindcss v3

--- src/theme/types/gradient-color.type.ts
+++
@@ -1,6 +0,0 @@
-import type { ColorEntriesType } from './color-entries.type';
-import type { ColorVariantType } from './color-variant.type';
-
-export type GradientColorType = {
-  readonly [x:ColorVariantType]: ColorEntriesType;
-};
